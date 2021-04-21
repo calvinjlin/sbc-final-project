dc5669f84ada2c1f6887848152d84ff855c2deb9
Calvin J Lin <58328424+calvinjlin@users.noreply.github.com>
2021-04-21 00:44:04 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Final project for SBC class using K-means clustering to analyze covid data.',
+    author='Calvin J Lin',
+    license='MIT',
+)
